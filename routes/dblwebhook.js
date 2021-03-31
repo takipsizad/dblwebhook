e3359aecdda141e8d35e88df30235407a81a8c2f
takipsizad <addeniz25@gmail.com>
2021-03-31 13:41:02 +0300

    Update dblwebhook.js

--- routes/dblwebhook.js
+++ routes/dblwebhook.js
@@ -1,20 +1,20 @@
-
+var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.json())
 router.use(express.json());
 require('dotenv').config();
 const webhook = process.env.webhook
-const { Webhook, MessageBuilder } = require('discord-webhook-node');
+const { Webhook,MessageBuilder } = require('discord-webhook-node');
 const hook = new Webhook(webhook);
 
 router.get('/', function(req, res) {
     res.send('working')
 })
 router.post('/', function(req, res) {
-  if (req.headers.authorization == process.env.secret) {
-	res.send('xd')
+  if (req.headers.authorization === process.env.secret) {
 	const user = req.body.user
+	console.log("vote")
 	const embed = new MessageBuilder()
 	.setTitle('thanks for voting')
 	.setColor('#00b0f4')
@@ -23,6 +23,7 @@ router.post('/', function(req, res) {
 	.setDescription(`<@${user}> Just Voted, thanks! Click [here]() to Vote!`)
 	.setAuthor('hey')
 	.setTimestamp();
+  res.send("xd")
 	hook.send(embed)
 		} else {
 			res.send('unautharized')
